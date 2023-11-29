9d7fee04c418d2b28a21b773dfec3da1377f3d90
Gertruda Čiučiulkaitė <gciuciulkaite@gmail.com>
2023-11-29 20:03:13 +0200

    Homework 3 done.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,15 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using Console = System.Console;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
 
-namespace BootCamp.Chapter
+namespace hw3
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
 
+            string namesurname = GetNameSurname();
+            int age = GetAge();
+            double weight = GetWeight();
+            double height = GetHeight();
+            double bmi = GetBMI(weight, height);
+            Console.WriteLine($"{namesurname} is {age} years old, his/her weight is {weight} kg and his/her height is {height} m.");
+            Console.WriteLine($"{namesurname} body-mass index (BMI) is  {bmi} .");
+        }
+        public static string GetNameSurname()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your surname: ");
+            string surname = Console.ReadLine();
+            return name + " " + surname;
+        }
+
+        public static int GetAge()
+        {
+            Console.Write("Enter your age: ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        public static double GetWeight()
+        {
+            Console.Write("Enter your weight (in kg): ");
+            return Convert.ToDouble(Console.ReadLine());
+        }
+        public static double GetHeight()
+        {
+            Console.Write("Enter your height (in m): ");
+            return Convert.ToDouble(Console.ReadLine());
+        }
+        public static double GetBMI(double weight, double height)
+        {
+            double bmi = weight / (height * height);
+            return bmi;
         }
     }
 }
